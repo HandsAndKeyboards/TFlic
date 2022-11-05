3bd4d55473ea9928372535a056ff80af1d9c3253
TotenTraum <81190163+TotenTraum@users.noreply.github.com>
2022-11-05 14:41:26 +0400

    added structure of src directory

---
+++ src/client/Command/RelayCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TFlic.Command
+{
+    internal class RelayCommand : Base.BaseCommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool>? canExecute;
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => execute?.Invoke(parameter);
+    }
+}
