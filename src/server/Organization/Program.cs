eb700829b091a4b065f57e8f7dab5fbe78314762
vany <ivev152@gmail.com>
2022-11-07 23:26:34 +0400

    теперь файл appsettings.json выбирается в зависимости от окружения

--- src/server/Organization/Program.cs
+++ src/server/Organization/Program.cs
@@ -1,25 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace Organization;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    /// <summary>
+    /// Конфигурация запуска приложения
+    /// </summary>
+    public static IConfiguration? AppConfiguration;
+    /*
+     * пришлось вынести конфигурацию в поле класса Program,
+     * так как не нашел другого способа обращаться к файлам
+     * appsettings.json / appsettings.Development.json в
+     * зависимости от переменной окружения 
+     */
+
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        AppConfiguration = builder.Configuration;
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
