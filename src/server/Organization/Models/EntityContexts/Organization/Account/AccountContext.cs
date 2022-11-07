86a5e3230ae7fddba7cb7a0b7080df5c0297f73f
vany <ivev152@gmail.com>
2022-11-07 23:26:08 +0400

    добавил служебные классы для работы с бд

---
+++ src/server/Organization/Models/EntityContexts/Organization/Account/AccountContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Organization.Models.EntityContexts.Organization.Account;
+
+public class AccountContext : DbContext
+{
+    public DbSet<Models.Organization.Accounts.Account> Accounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(Program.AppConfiguration?.GetConnectionString("DbConnectionString"));
+    }
+}
---
+++ src/server/Organization/Models/EntityContexts/Organization/OrganizationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Organization.Models.EntityContexts.Organization;
+
+public class OrganizationContext : DbContext
+{
+    public DbSet<Models.Organization.Organization> Organizations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(Program.AppConfiguration?.GetConnectionString("DbConnectionString"));
+    }
+}
