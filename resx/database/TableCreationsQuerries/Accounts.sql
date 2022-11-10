ff91603b286a64582a99e9d5c751d767425296ed
vany <ivev152@gmail.com.com>
2022-11-10 14:56:26 +0400

    оформил тестовую бд

--- resx/database/TableCreationsQuerries/Accounts.sql
+++ resx/database/TableCreationsQuerries/Accounts.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Accounts (
     "Id" INTEGER,
+    "Name" VARCHAR(50) NOT NULL,
     "Login" VARCHAR(50) NOT NULL,
     "PasswordHash" BLOB NOT NULL,
-    "Name" VARCHAR(50) NOT NULL,
 
     PRIMARY KEY ("Id")
 )
--- resx/database/TablesFillingQuerries/UserGroupsAccounts.sql
+++ resx/database/TablesFillingQuerries/UserGroupsAccounts.sql
@@ -1,29 +1,76 @@
 ﻿------------------------ ORG 1
+---- Без роли
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId) 
-VALUES (0, 1);
+VALUES (1, 5);
 
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (1, 1);
+VALUES (1, 12);
 
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (1, 18);
+
+---- Админы
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
 VALUES (2, 1);
 
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (2, 4);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (2, 6);
+
+---- Участники проектов
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (3, 1);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (3, 7);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (3, 13);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (3, 19);
+
 ------------------------ ORG 2
+---- Админы
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (0, 2);
+VALUES (5, 2);
+
+---- Участники проектов
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (6, 8);
 
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (1, 2);
+VALUES (6, 14);
 
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (2, 2);
+VALUES (6, 20);
 
 ------------------------ ORG 3
+---- Без роли
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (0, 3);
+VALUES (7, 15);
+
+---- Админы
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (8, 2);
 
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (1, 3);
+VALUES (8, 3);
 
 INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
-VALUES (2, 3);
+VALUES (8, 16);
+
+---- Участники проектов
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (9, 9);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (9, 10);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (9, 11);
+
+INSERT INTO UserGroupsAccounts (UserGroupId, AccountId)
+VALUES (9, 17);
