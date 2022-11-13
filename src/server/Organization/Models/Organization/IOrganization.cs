4262bcd0fe0c2d86bfa9d8120dec2a2e9e1dd327
vany <ivev152@gmail.com>
2022-11-14 01:05:49 +0400

    интерфейс организации теперь модержит методы для добавления и удаления указанных аккаунтов из указанных групп пользователей + причесан код

--- src/server/Organization/Models/Organization/IOrganization.cs
+++ src/server/Organization/Models/Organization/IOrganization.cs
@@ -34,4 +34,37 @@ public interface IOrganization : IUserAggregator
     /// Группы пользователй организации
     /// </summary>
     IReadOnlyCollection<IUserGroup> UserGroups { get; init; }
+
+    /// <summary>
+    /// Метод добавляет указанного пользователя в группу пользователей
+    /// с указанным идентификатором группы в организации
+    /// </summary>
+    /// <param name="account">Добавляемый аккаунт</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    void AddAccountToGroup(IAccount account, long groupLocalId);
+
+    /// <summary>
+    /// Метод добавляет пользователя с указанным уникальным идентификатором
+    /// в группу пользователей с указанным локальным идентификатором группы
+    /// </summary>
+    /// <remarks></remarks>
+    /// <param name="accountId">Уникальный идентификатор добавляемого аккаунта</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    void AddAccountToGroup(long accountId, long groupLocalId);
+
+    /// <summary>
+    /// Метод удаляе указанный аккаунт из группы пользователей
+    /// с указанным локальным идентификатором группы
+    /// </summary>
+    /// <param name="account">Удаляемый аккаунт</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    void RemoveAccountFromGroup(IAccount account, long groupLocalId);
+
+    /// <summary>
+    /// Метод удаляет аккаунт с указанным уникальным идентификатором из
+    /// группы пользователей с указанным локальным идентификатором группы
+    /// </summary>
+    /// <param name="accountId">Уникальный идентификатор удаляемого аккаунта</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    void RemoveAccountFromGroup(long accountId, long groupLocalId);
 }
--- src/server/Organization/Models/Organization/Organization.cs
+++ src/server/Organization/Models/Organization/Organization.cs
@@ -8,8 +8,8 @@ public class Organization : IOrganization
 {
     public class OrganizationException : Exception
     {
-            public OrganizationException(string message) : base(message) { }
-            public OrganizationException() { }
+        public OrganizationException(string message) : base(message) { }
+        public OrganizationException() { }
     }
     
     /// <summary>
@@ -21,6 +21,14 @@ public class Organization : IOrganization
 
     #region Methods
 
+    /// <summary>
+    /// Метод добавляет аккаунт в группу пользователей "Пользователи без роли" 
+    /// </summary>
+    /// <param name="account">Добавляемый аккаунт</param>
+    /// <returns>true в случае успешного добавления, иначе - false</returns>
+    /// <exception cref="OrganizationException">
+    /// Исключение вазникает в случае отсутствия в организации группы пользователей "Пользователи без роли"
+    /// </exception>
     public bool AddAccount(IAccount account)
     {
         if (Contains(account)) { return false; }
@@ -33,6 +41,10 @@ public class Organization : IOrganization
         return true;
     }
     
+    /// <summary>
+    /// Метод удаляет указанный аккаунт из всех групп пользователей организации
+    /// </summary>
+    /// <returns>true в случае успешного удаления, иначе - false</returns>
     public bool RemoveAccount(IAccount account)
     {
         var toRemove = Contains(account.Id);
@@ -40,6 +52,11 @@ public class Organization : IOrganization
                _userGroups.Aggregate(false, (current, userGroup) => current || userGroup.RemoveAccount(toRemove));
     }
     
+    /// <summary>
+    /// Метод удаляет аккаунт с уаазаннм уникальным идентификатором из всех групп пользователей организации
+    /// </summary>
+    /// <param name="id">Уникальный идентификатор удаляемого аккаунта</param>
+    /// <returns>Ссылка на удаляемый объект в случае успешного удаления, иначе - null</returns>
     public IAccount? RemoveAccount(long id)
     {
         var toRemove = Contains(id);
@@ -51,11 +68,22 @@ public class Organization : IOrganization
         return toRemove;
     }
     
+    /// <summary>
+    /// Метод проверяет, содержит ли любая из групп пользователей
+    /// организации пользователя с указанным идентификатором
+    /// </summary>
+    /// /// <returns>true в случае успушного удаление, иначе - false</returns>
     public bool Contains(IAccount account)
     {
         return _userGroups.Any(userGroup => userGroup.Contains(account));
     }
     
+    /// <summary>
+    /// Метод проверяет, содержит ли любая из групп пользователей
+    /// организации пользователя с указанным идентификатором
+    /// </summary>
+    /// <param name="id">Уникальный идентификатор проверяемого пользователя</param>
+    /// <returns>ссылка на объект, если он присутствует, иначе - null </returns>
     public IAccount? Contains(long id)
     {
         IAccount? account = null;
@@ -102,10 +130,114 @@ public class Organization : IOrganization
         init => _archivedProjects = (List <IProject>) value;
     }
 
+    /// <summary>
+    /// Группы пользователй организации
+    /// </summary>
+    /// <remarks>
+    /// Свойство не должно использоваться для перемещения аккаунтов между группами пользователей
+    /// </remarks>
     public IReadOnlyCollection<IUserGroup> UserGroups
     {
         get => _userGroups;
         init => _userGroups = (List<IUserGroup>) value;
+    }
+
+    /// <summary>
+    /// Метод добавляет указанного пользователя в группу пользователей
+    /// с указанным идентификатором группы в организации
+    /// </summary>
+    /// <param name="account">Добавляемый аккаунт</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    /// <exception cref="OrganizationException">
+    /// <list type="bullet">
+    ///     <listheader>Возникает в случаях:</listheader>
+    ///     <item>Организация не содержит указанный аккаунт</item>
+    ///     <item>Организация не содержит группу пользователей с указанным локальным Id</item>
+    ///     <item>Организация не содержит группу пользователей "Пользователи без роли"</item>
+    /// </list>
+    /// </exception>
+    public void AddAccountToGroup(IAccount account, long groupLocalId)
+    {
+        if (!Contains(account)) { throw new OrganizationException($"Организация не содержит аккаунт с Id = {account.Id}"); }
+        
+        var localUserGroup = _userGroups.Find(userGroup => userGroup.LocalId == groupLocalId);
+        if (localUserGroup is null) { throw new OrganizationException($"Организация не содержит группу пользователей с локальным Id = {groupLocalId}"); }
+
+        localUserGroup.AddAccount(account);
+        
+        /*
+         * удаляем аккаунт из группы "Без роли" так как аккаунт может
+         * одновременно содержаться в группе "Без роли" и в какой-либо другой
+         */
+        var noRoleUserGroup = _userGroups.Find(userGroup => userGroup.LocalId == (int) PrimaryUserGroups.NoRole);
+        if (noRoleUserGroup is null) { throw new OrganizationException("Не найдена группа пользователей 'Пользователи без роли'"); }
+        noRoleUserGroup.RemoveAccount(account);
+    }
+    
+    /// <summary>
+    /// Метод добавляет пользователя с указанным уникальным идентификатором
+    /// в группу пользователей с указанным локальным идентификатором группы
+    /// </summary>
+    /// <param name="accountId">Уникальный идентификатор добавляемого аккаунта</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    /// <exception cref="OrganizationException">
+    /// Возникает в случае, если аккаунт с указанным accountId не содержится в организации
+    /// </exception>
+    public void AddAccountToGroup(long accountId, long groupLocalId)
+    {
+        var account = Contains(accountId);
+        if (account is null) { throw new OrganizationException($"Организация не содержит аккаунт с Id = {accountId}"); }
+        
+        AddAccountToGroup(account, groupLocalId);
+    }
+    
+    /// <summary>
+    /// Метод удаляе указанный аккаунт из группы пользователей
+    /// с указанным локальным идентификатором группы
+    /// </summary>
+    /// <param name="account">Удаляемый аккаунт</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    /// <exception cref="OrganizationException">
+    /// <list type="bullet">
+    ///     <listheader>Возникает в случаях:</listheader>
+    ///     <item>Организация не содержит указанный аккаунт</item>
+    ///     <item>Организация не содержит группу пользователей с указанным локальным Id</item>
+    ///     <item>Организация не содержит группу пользователей "Пользователи без роли"</item>
+    /// </list>
+    /// </exception>
+    public void RemoveAccountFromGroup(IAccount account, long groupLocalId)
+    {
+        if (!Contains(account)) { throw new OrganizationException($"Организация не содержит аккаунт с Id = {account.Id}"); }
+        
+        var localUserGroup = _userGroups.Find(userGroup => userGroup.LocalId == groupLocalId);
+        if (localUserGroup is null) { throw new OrganizationException($"Организация не содержит группу пользователей с локальным Id = {groupLocalId}"); }
+
+        localUserGroup.RemoveAccount(account);
+        
+        /*
+         * если после удаление не содержится ни в одной группе пользователей организации,
+         * добавляем его в группу "Пользователи без роли"
+         */
+        var noRoleUserGroup = _userGroups.Find(userGroup => userGroup.LocalId == (int) PrimaryUserGroups.NoRole);
+        if (noRoleUserGroup is null) { throw new OrganizationException("Не найдена группа пользователей 'Пользователи без роли'"); }
+        if (!Contains(account)) noRoleUserGroup.AddAccount(account);
+    }
+
+    /// <summary>
+    /// Метод удаляет аккаунт с указанным уникальным идентификатором из
+    /// группы пользователей с указанным локальным идентификатором группы
+    /// </summary>
+    /// <param name="accountId">Уникальный идентификатор удаляемого аккаунта</param>
+    /// <param name="groupLocalId">Идентификатор группы пользователей в организации</param>
+    /// <exception cref="OrganizationException">
+    /// Возникает в случае, если аккаунт с указанным accountId не содержится в организации
+    /// </exception>
+    public void RemoveAccountFromGroup(long accountId, long groupLocalId)
+    {
+        var account = Contains(accountId);
+        if (account is null) { throw new OrganizationException($"Организация не содержит аккаунт с Id = {accountId}"); }
+        
+        RemoveAccountFromGroup(account, groupLocalId);
     }
     
     #endregion
@@ -121,7 +253,6 @@ public class Organization : IOrganization
 
     private readonly List<IProject> _activeProjects = new ();
     private readonly List<IProject> _archivedProjects = new ();
-    
     private readonly List<IUserGroup> _userGroups = new ();
 
     #endregion
