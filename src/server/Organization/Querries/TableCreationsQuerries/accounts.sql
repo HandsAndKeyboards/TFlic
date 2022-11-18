eac7b5605bffe6459baa5e6c112d15175d887158
vany <ivev152@gmail.com>
2022-11-18 22:47:15 +0400

    теперь атрибут "login" уникальный

--- src/server/Organization/Querries/TableCreationsQuerries/accounts.sql
+++ src/server/Organization/Querries/TableCreationsQuerries/accounts.sql
@@ -1,7 +1,7 @@
 create table accounts (
     id bigserial,
     name varchar(50) not null,
-    login varchar(50) not null,
+    login varchar(50) not null unique,
     password_hash bytea not null,
 
     primary key (id)
