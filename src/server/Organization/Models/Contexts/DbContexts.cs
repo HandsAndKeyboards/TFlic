70d8d903aff9e343e1c64b65df3296199bcb1706
vany <ivev152@gmail.com>
2022-11-22 15:29:40 +0400

    удалил нерекомендованные для использования свойства

--- src/server/Organization/Models/Contexts/DbContexts.cs
+++ src/server/Organization/Models/Contexts/DbContexts.cs
@@ -20,38 +20,6 @@ public static class DbContexts
     
     public static string DbConnectionString { get; set; } = string.Empty;
 
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static AccountContext AccountContext =>
-        new (CreateOptions<AccountContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static UserGroupContext UserGroupContext =>
-        new (CreateOptions<UserGroupContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static OrganizationContext OrganizationContext =>
-        new (CreateOptions<OrganizationContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static ProjectContext ProjectContext =>
-        new (CreateOptions<ProjectContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static BoardContext BoardContext =>
-        new (CreateOptions<BoardContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static ColumnContext ColumnContext =>
-        new (CreateOptions<ColumnContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static TaskContext TaskContext =>
-        new (CreateOptions<TaskContext>(DbConnectionString).Options);
-    
-    [Obsolete("В скором времени свойство будет удалено")]
-    public static ComponentContext ComponentContext =>
-        new (CreateOptions<ComponentContext>(DbConnectionString).Options);
-    
     private static DbContextOptionsBuilder<T> CreateOptions<T>(string dbConnectionString) where T : DbContext => 
         new DbContextOptionsBuilder<T>().UseNpgsql(dbConnectionString);
 }
