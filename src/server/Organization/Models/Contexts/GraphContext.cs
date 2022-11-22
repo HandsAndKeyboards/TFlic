1ce37e789b295fb91a1230d9539ef3df3f8e7cf1
TotenTraum <81190163+TotenTraum@users.noreply.github.com>
2022-11-22 20:11:03 +0400

    Закомментировал сломанный код и удалил лишний файл

--- src/server/Organization/Models/Contexts/GraphContext.cs
+++ src/server/Organization/Models/Contexts/GraphContext.cs
@@ -1,10 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace Organization.Models.Contexts;
-
-public class GraphContext : DbContext
-{
-    public DbSet<Graph> Graphs { get; set; } = null!;
-
-    public GraphContext(DbContextOptions<GraphContext> options) : base(options) { }
-}
+// using Microsoft.EntityFrameworkCore;
+//
+// namespace Organization.Models.Contexts;
+//
+// public class GraphContext : DbContext
+// {
+//     public DbSet<Graph> Graphs { get; set; } = null!;
+//
+//     public GraphContext(DbContextOptions<GraphContext> options) : base(options) { }
+// }
