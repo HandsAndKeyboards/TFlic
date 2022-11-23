2a31cac67755f5e6bd5b59ea03bcd82af0f21e1a
dacsson <sda20036@gmail.com>
2022-11-23 03:55:51 +0300

    + прототипы контроллера графа, контекста графа для бд

--- src/server/Organization/Models/Contexts/GraphContext.cs
+++ src/server/Organization/Models/Contexts/GraphContext.cs
@@ -1,10 +1,11 @@
-// using Microsoft.EntityFrameworkCore;
-//
-// namespace Organization.Models.Contexts;
-//
-// public class GraphContext : DbContext
-// {
-//     public DbSet<Graph> Graphs { get; set; } = null!;
-//
-//     public GraphContext(DbContextOptions<GraphContext> options) : base(options) { }
-// }
+using Microsoft.EntityFrameworkCore;
+using Organization.Models.Organization.Graphs;
+
+namespace Organization.Models.Contexts;
+
+public class GraphContext : DbContext
+{
+    public DbSet<GraphAggregator> Graphs { get; set; } = null!;
+
+    public GraphContext(DbContextOptions<GraphContext> options) : base(options) { }
+}
--- src/server/Organization/Models/Organization/Graphs/GraphAggregator.cs
+++ src/server/Organization/Models/Organization/Graphs/GraphAggregator.cs
@@ -1,102 +1,98 @@
-﻿// using Organization.Models.Organization.Project;
-//
-// namespace Organization.Models.Organization.Graphs
-// {
-//     public class GraphAggregator : IGraphAggregator
-//     {
-//         #region Public
-//
-//         public GraphAggregator(string _GraphType, Sprint _Sprint)
-//         {
-//             GraphType = _GraphType; 
-//             TeamSprint = _Sprint;
-//         }
-//
-//         #region Methods
-//
-//         /// <summary>
-//         /// Формирует массив подписей для графа на оси ОХ
-//         /// </summary>
-//         /// <param name="sprint">Спринт на основе которого строится график</param>
-//         public void FormXLabels()
-//         {
-//             XLabels = new List<string>();
-//             switch (GraphType)
-//             {
-//                 case GraphType[0]:
-//                     break;
-//                 case GraphType[1]:
-//                     break;
-//                 case GraphType[2]:
-//                     throw new ArgumentException("UserWorkload diagram do not support this method");
-//                 default:
-//                     throw new ArgumentException("Invalid argument for this method");
-//             }
-//         }
-//
-//         /// <summary>
-//         /// Формирует массив подписей для графа на оси ОХ
-//         /// </summary>
-//         /// <param name="sprint">Спринт на основе которого строится график</param>
-//         public void FormYValues()
-//         {
-//             YValues = new List<double>();
-//             switch (GraphType)
-//             {
-//                 case GraphType[0]:
-//                     break;
-//                 case GraphType[1]:
-//                     break;
-//                 case GraphType[2]:
-//                     throw new ArgumentException("UserWorkload diagram do not support this method");
-//                 default:
-//                     throw new ArgumentException("Invalid argument for this method");
-//             }
-//         }
-//         #endregion
-//
-//         #endregion
-//
-//         #region Private
-//
-//         #region Methods
-//
-//         /// <summary>
-//         /// Суммирует "примерное время выполнения" задачи, указанное разработчиками
-//         /// </summary>
-//         /// <param name="sprint">Спринт на основе которого строится график</param>
-//         private double AggregateEstimatedTime()
-//         {
-//             double AggregatedTime = 0;
-//             // - TODO
-//             return AggregatedTime;
-//         }
-//
-//         /// <summary>
-//         /// Суммирует "реальное время выполнения" задачи, указанное разработчиками
-//         /// </summary>
-//         /// <param name="sprint">Спринт на основе которого строится график</param>
-//         private double AggregateRealTime()
-//         {
-//             double AggregatedTime = 0;
-//             // - TODO
-//             return AggregatedTime;
-//         }
-//
-//         #endregion
-//
-//         #region Fields
-//
-//         private readonly string[] GraphTypes = { "Burndown", "TeamSpeed", "UsersWorkload" }; 
-//         
-//         #endregion
-//         
-//         #endregion
-//
-//         public IReadOnlyCollection<double> ChartValues { get; set; }
-//         public IReadOnlyCollection<string> XLabels { get; set; }
-//         public IReadOnlyCollection<double> YValues { get; set; }
-//         public string GraphType { get; set; }
-//         public Sprint TeamSprint { get; set; }
-//     }
-// }
+﻿using Organization.Models.Organization.Project;
+
+namespace Organization.Models.Organization.Graphs
+{
+    public class GraphAggregator : IGraphAggregator
+    {
+        #region Public
+
+        public GraphAggregator(string _GraphType, Sprint _Sprint)
+        {
+            GraphType = _GraphType;
+            TeamSprint = _Sprint;
+        }
+
+        #region methods
+
+        /// <summary>
+        /// формирует массив подписей для графа на оси ох
+        /// </summary>
+        public void FormXLabels()
+        {
+            XLabels = new List<string>();
+            switch (GraphType)
+            {
+                case "Burndown":
+                    break;
+                case "TeamSpeed":
+                    break;
+                case "UserWorkload":
+                    throw new ArgumentException("userworkload diagram do not support this method");
+                default:
+                    throw new ArgumentException("invalid argument for this method");
+            }
+        }
+
+        /// <summary>
+        /// формирует массив подписей для графа на оси ох
+        /// </summary>
+        public void FormYValues()
+        {
+            YValues = new List<double>();
+            switch (GraphType)
+            {
+                case "Burndown":
+                    break;
+                case "TeamSpeed":
+                    break;
+                case "UserWorkload":
+                    throw new ArgumentException("userworkload diagram do not support this method");
+                default:
+                    throw new ArgumentException("invalid argument for this method");
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region private
+
+        #region methods
+
+        /// <summary>
+        /// суммирует "примерное время выполнения" задачи, указанное разработчиками
+        /// </summary>
+        private double AggregateEstimatedTime()
+        {
+            double aggregatedtime = 0;
+            // - todo
+            return aggregatedtime;
+        }
+
+        /// <summary>
+        /// суммирует "реальное время выполнения" задачи, указанное разработчиками
+        /// </summary>
+        private double AggregateRealTime()
+        {
+            double aggregatedtime = 0;
+            // - todo
+            return aggregatedtime;
+        }
+
+        #endregion
+
+        #region fields
+
+        private readonly string[] GraphTypes = new string[] { "burndown", "teamspeed", "usersworkload" };
+
+        #endregion
+
+        #endregion
+
+        public IReadOnlyCollection<double> ChartValues { get; set; }
+        public IReadOnlyCollection<string> XLabels { get; set; }
+        public IReadOnlyCollection<double> YValues { get; set; }
+        public string GraphType { get; set; }
+        public Sprint TeamSprint { get; set; }
+    }
+}
