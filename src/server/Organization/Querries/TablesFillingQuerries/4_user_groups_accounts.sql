1fee0734fbf1696efb3fd5e2362d0adca5e9a929
vany <ivev152@gmail.com>
2022-11-25 02:00:30 +0400

    для более удобной передачи по сети  поменял локальные Id групп пользователей

--- src/server/Organization/Querries/TablesFillingQuerries/4_user_groups_accounts.sql
+++ src/server/Organization/Querries/TablesFillingQuerries/4_user_groups_accounts.sql
@@ -9,68 +9,68 @@ values (1, 12);
 insert into user_groups_accounts (user_group_id, account_id)
 values (1, 18);
 
----- Админы
+---- Участники проектов
 insert into user_groups_accounts (user_group_id, account_id)
 values (2, 1);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (2, 4);
+values (2, 7);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (2, 6);
+values (2, 13);
 
----- Участники проектов
+insert into user_groups_accounts (user_group_id, account_id)
+values (2, 19);
+
+---- Админы
 insert into user_groups_accounts (user_group_id, account_id)
 values (3, 1);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (3, 7);
+values (3, 4);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (3, 13);
-
-insert into user_groups_accounts (user_group_id, account_id)
-values (3, 19);
+values (3, 6);
 
 ------------------------ ORG 2
----- Админы
-insert into user_groups_accounts (user_group_id, account_id)
-values (5, 2);
-
 ---- Участники проектов
 insert into user_groups_accounts (user_group_id, account_id)
-values (6, 8);
+values (5, 8);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (6, 14);
+values (5, 14);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (6, 20);
+values (5, 20);
+
+---- Админы
+insert into user_groups_accounts (user_group_id, account_id)
+values (6, 2);
 
 ------------------------ ORG 3
 ---- Без роли
 insert into user_groups_accounts (user_group_id, account_id)
 values (7, 15);
 
----- Админы
-insert into user_groups_accounts (user_group_id, account_id)
-values (8, 2);
-
-insert into user_groups_accounts (user_group_id, account_id)
-values (8, 3);
-
-insert into user_groups_accounts (user_group_id, account_id)
-values (8, 16);
-
 ---- Участники проектов
 insert into user_groups_accounts (user_group_id, account_id)
-values (9, 9);
+values (8, 8);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (9, 10);
+values (8, 10);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (9, 11);
+values (8, 11);
 
 insert into user_groups_accounts (user_group_id, account_id)
-values (9, 17);
+values (8, 17);
+
+---- Админы
+insert into user_groups_accounts (user_group_id, account_id)
+values (9, 2);
+
+insert into user_groups_accounts (user_group_id, account_id)
+values (9, 3);
+
+insert into user_groups_accounts (user_group_id, account_id)
+values (9, 16);
