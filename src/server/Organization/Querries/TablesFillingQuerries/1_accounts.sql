c4a0df5a5fdf03771f0ae30936084445ab9685bc
vany <ivev152@gmail.com>
2022-11-26 00:44:04 +0400

    для аутентификации разбил таблицу account на две: accounts & auth_info

--- src/server/Organization/Querries/TablesFillingQuerries/1_accounts.sql
+++ src/server/Organization/Querries/TablesFillingQuerries/1_accounts.sql
@@ -1,79 +1,139 @@
 ﻿-- pass - 120
-insert into accounts (login, password_hash, name)
-values ('vany', 'KrrKSRHmj6m/vzSC7nl/1bkEW4Qf3/clNVfF/hXeZHc=', 'Иван Ермолаев');
+insert into accounts (name)
+values ('Иван Ермолаев');
+
+insert into auth_info (account_id, login, password_hash) 
+values ((select max(id) from accounts), 'vany', 'KrrKSRHmj6m/vzSC7nl/1bkEW4Qf3/clNVfF/hXeZHc=');
 
 -- pass - 121
-insert into accounts (login, password_hash, name)
-values ('roline', 'iaoeWAAjci22dkboFJ6yRsdI4YDjShz2easLQaQW2QQ=', 'Роман Линев');
+insert into accounts (name)
+values ('Роман Линев');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'roline', 'iaoeWAAjci22dkboFJ6yRsdI4YDjShz2easLQaQW2QQ=');
 
 -- pass - 122
-insert into accounts (login, password_hash, name)
-values ('traum', 'G+ADQQguJcTiUcpnE+dn9xMaKCOwBSyvnJsAbsUS9ss=', 'Азим Усманов');
+insert into accounts (name)
+values ('Азим Усманов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'traum', 'G+ADQQguJcTiUcpnE+dn9xMaKCOwBSyvnJsAbsUS9ss=');
 
 -- pass - 123
-insert into accounts (login, password_hash, name)
-values ('dacsson', 'pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=', 'Артемий Сафонов');
+insert into accounts (name)
+values ('Артемий Сафонов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'dacsson', 'pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=');
 
 -- pass - 124
-insert into accounts (login, password_hash, name)
-values ('lis_reineke', 'av/a47PBqmqnaJ6bansyJaY2qhrAAl9JDMoShc6vFIc=', 'Ярослав Музыченко');
+insert into accounts (name)
+values ('Ярослав Музыченко');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'lis_reineke', 'av/a47PBqmqnaJ6bansyJaY2qhrAAl9JDMoShc6vFIc=');
 
 -- pass - 125
-insert into accounts (login, password_hash, name)
-values ('extremepum', 'D47zN3sw/Ef5a0gkf0Y6cmqAL2Lz+qA9VkA3UdL2bGc=', 'Петр Денисов');
+insert into accounts (name)
+values ('Петр Денисов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'extremepum', 'D47zN3sw/Ef5a0gkf0Y6cmqAL2Lz+qA9VkA3UdL2bGc=');
 
 -- pass - 126
-insert into accounts (login, password_hash, name)
-values ('daniilpospelov', 'ZaaZkFwCYZNwvPkgf1pHfD1nEwynHsb3UOB/6NUQsIQ=', 'Даниил Поспелов');
+insert into accounts (name)
+values ('Даниил Поспелов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'daniilpospelov', 'ZaaZkFwCYZNwvPkgf1pHfD1nEwynHsb3UOB/6NUQsIQ=');
 
 -- pass - 127
-insert into accounts (login, password_hash, name)
-values ('belousov', 'kix5VCFsz+emHe9gkwXOHcfGfiJfhz8lbTDXqO5PQEw=', 'Тимофей Белоусов');
+insert into accounts (name)
+values ('Тимофей Белоусов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'belousov', 'kix5VCFsz+emHe9gkwXOHcfGfiJfhz8lbTDXqO5PQEw=');
 
 -- pass - 128
-insert into accounts (login, password_hash, name)
-values ('marphu', 'J0e3xxhWS6XwZvBSOwPhf2pJawaFEzPS1Zq22GMiWEg=', 'Александр Яроцкий');
+insert into accounts (name)
+values ('Александр Яроцкий');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'marphu', 'J0e3xxhWS6XwZvBSOwPhf2pJawaFEzPS1Zq22GMiWEg=');
 
 -- pass - 129
-insert into accounts (login, password_hash, name)
-values ('uspenskiy', 'ZWYjDjo843dMG7x8GLWQrg9Fe7zVEekOPn3KKgLnrdw=', 'Владимир Успенский');
+insert into accounts (name)
+values ('Владимир Успенский');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'uspenskiy', 'ZWYjDjo843dMG7x8GLWQrg9Fe7zVEekOPn3KKgLnrdw=');
 
 -- pass - 130
-insert into accounts (login, password_hash, name)
-values ('nanogus', 'ONZtlpKsWQAAqRsDqI2hyI1R+rK3j2MXH1U+zFUaDG8=', 'Сергей Плищенко');
+insert into accounts (name)
+values ('Сергей Плищенко');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'nanogus', 'ONZtlpKsWQAAqRsDqI2hyI1R+rK3j2MXH1U+zFUaDG8=');
 
 -- pass - 131
-insert into accounts (login, password_hash, name)
-values ('hunsil', '7sqR/UObbV6Cfo/af+41BG8t75NQhjdIP2voot96Q5I=', 'Андрей Сондырев');
+insert into accounts (name)
+values ('Андрей Сондырев');
 
--- pass - 132/
-insert into accounts (login, password_hash, name)
-values ('kristig00d', 'iaoeWAAjci22dkboFJ6yRsdI4YDjShz2easLQaQW2QQ=', 'Кристина Максимова');
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'hunsil', '7sqR/UObbV6Cfo/af+41BG8t75NQhjdIP2voot96Q5I=');
+
+-- pass - 132
+insert into accounts (name)
+values ('Кристина Максимова');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'kristig00d', 'iaoeWAAjci22dkboFJ6yRsdI4YDjShz2easLQaQW2QQ=');
 
 -- pass - 133
-insert into accounts (login, password_hash, name)
-values ('biomix', '0vSDZywCOfbX3TyezuberLzVkYWFViWQKoscGjvWdEA=', 'Денис Иванов');
+insert into accounts (name)
+values ('Денис Иванов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'biomix', '0vSDZywCOfbX3TyezuberLzVkYWFViWQKoscGjvWdEA=');
 
 -- pass - 134
-insert into accounts (login, password_hash, name)
-values ('edemina86', 'XTifXi40xrC62WWBwizuC+Ntz2J81zr01MzKzZ70DMM=', 'Екатерина Андреевна');
+insert into accounts (name)
+values ('Екатерина Андреевна');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'edemina86', 'XTifXi40xrC62WWBwizuC+Ntz2J81zr01MzKzZ70DMM=');
 
 -- pass - 135
-insert into accounts (login, password_hash, name)
-values ('zyatikov', 'E2cQd7ZqKYdKJXi1JAMZCS7yoQQyKOQz6bAGteU+dRM=', 'Антон Зятиков');
+insert into accounts (name)
+values ('Антон Зятиков');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'zyatikov', 'E2cQd7ZqKYdKJXi1JAMZCS7yoQQyKOQz6bAGteU+dRM=');
 
 -- pass - 136
-insert into accounts (login, password_hash, name)
-values ('sarapaev', 'NuviBbzfxJmiXmkj9EUPqNSBls60+gzgd9nY7Eo2km0=', 'Митяй Сарапаев');
+insert into accounts (name)
+values ('Митяй Сарапаев');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'sarapaev', 'NuviBbzfxJmiXmkj9EUPqNSBls60+gzgd9nY7Eo2km0=');
 
 -- pass - 137
-insert into accounts (login, password_hash, name)
-values ('markmikhailov', '2A6ubpbRSLOyq7vGdgB3tmxOoHH4R9q1c9UHoyxNmaU=', 'Марк Михайлов');
+insert into accounts (name)
+values ('Марк Михайлов');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'markmikhailov', '2A6ubpbRSLOyq7vGdgB3tmxOoHH4R9q1c9UHoyxNmaU=');
 
 -- pass - 138
-insert into accounts (login, password_hash, name)
-values ('kosmoss', '1qQDFzNhC7CA0L+nlPzJ29z/dINK6qt8a5J+Iel1QDc=', 'Космос Фортранович');
+insert into accounts (name)
+values ('Космос Фортранович');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'kosmoss', '1qQDFzNhC7CA0L+nlPzJ29z/dINK6qt8a5J+Iel1QDc=');
 
 -- pass - 139
-insert into accounts (login, password_hash, name)
-values ('kashirinav', 'jSe6N8XYEBBrVfP9bNs1hCAH6IdUGEv8DmA1+bzt5jM=', 'Артём Каширин');
+insert into accounts (name)
+values ('Артём Каширин');
+
+insert into auth_info (account_id, login, password_hash)
+values ((select max(id) from accounts), 'kashirinav', 'jSe6N8XYEBBrVfP9bNs1hCAH6IdUGEv8DmA1+bzt5jM=');
