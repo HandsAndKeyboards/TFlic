a56f6cbe86563777a22368460df54d7cc93573f1
Артём Сафонов <sda20036@gmail.com>
2022-12-02 17:35:19 +0300

    + Окно для отчётов (1-ая половина изменений)

---
+++ src/client/ViewModel/BurndownViewModel.cs
@@ -0,0 +1,100 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TFlic.Command;
+using TFlic.ViewModel.ViewModelClass;
+
+namespace TFlic.ViewModel
+{
+    internal class BurndownViewModel : Base.ViewModelBase
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+
+        #region Fields
+
+        int indexSprint = 0;
+        public int IndexSprint
+        {
+            get => indexSprint;
+            set => Set(ref indexSprint, value);
+        }
+
+        ObservableCollection<Sprint> sprints = new();
+        public ObservableCollection<Sprint> Sprints
+        {
+            get => sprints;
+            set => Set(ref sprints, value);
+        }
+        #endregion
+
+
+        #region Commands
+
+        #region Команда выбора спринта
+
+        string sprintName = string.Empty;
+        public string SprintName
+        {
+            get => sprintName;
+            set => Set(ref sprintName, value);
+        }
+
+        public ICommand ChooseSprintCommand { get; }
+        private void OnChooseSprintCommandExecuted(object p)
+        {
+            sprints.Add(
+                new Sprint()
+                {
+                    Name = sprintName,
+                }
+            );
+        }
+        private bool CanChooseSprintCommandExecute(object p) { return true; }
+
+        #endregion
+
+        #endregion
+
+        #region Constructors
+
+        public BurndownViewModel()
+        {
+            ChooseSprintCommand =
+                new RelayCommand(OnChooseSprintCommandExecuted);
+
+            TestData();
+        }
+
+        #endregion
+
+        #region Tests
+
+        public void TestData()
+        {
+            sprints.Add(
+                new()
+                {
+                    Name = "1"
+                }
+            ); 
+
+            sprints.Add(
+                new()
+                {
+                    Name = "2"
+                }
+            ); 
+        }
+
+        #endregion
+    }
+}
---
+++ src/client/ViewModel/ViewModelClass/Sprint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFlic.ViewModel.ViewModelClass
+{
+    internal class Sprint
+    {
+        #region Overriding INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+
+        #region Fields and properties
+        string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+        #endregion
+    }
+}
