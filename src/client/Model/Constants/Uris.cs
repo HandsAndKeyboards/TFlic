49877c5a05e4f3e4fc56becd658a9afff04f04de
vany <ivev152@gmail.com>
2022-12-09 19:54:23 +0400

    сделал статическую обертку для клиента

--- src/client/Model/Constants/Uris.cs
+++ src/client/Model/Constants/Uris.cs
@@ -2,8 +2,5 @@
 
 public static class Uris
 {
-    // todo чтение из файла
-    public const string LoginUri = "https://localhost:44303/login";
-    public const string RegisterUri = "https://localhost:44303/register";
-    public const string RefreshUri = "https://localhost:44303/refresh";
+    public const string BaseUrl = "https://localhost:44303";
 }
---
+++ src/client/Model/WebClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+using TFlic.Model.Constants;
+
+namespace TFlic.Model;
+
+public static class WebClient
+{
+    public static Client Get { get; } = new (Uris.BaseUrl, new HttpClient());
+}
