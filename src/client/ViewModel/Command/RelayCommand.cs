26b11925c9c9dee560a319bde798f18d3742fe07
vany <ivev152@gmail.com>
2022-12-09 21:23:04 +0400

    навел порядок

--- src/client/ViewModel/Command/RelayCommand.cs
+++ src/client/ViewModel/Command/RelayCommand.cs
@@ -5,17 +5,20 @@ namespace TFlic.ViewModel.Command
 {
     internal class RelayCommand : BaseCommand
     {
-        private readonly Action<object> execute;
-        private readonly Func<object, bool>? canExecute;
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool>? _canExecute;
 
-        public RelayCommand(Action<object> Execute, Func<object, bool>? CanExecute = null)
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
         {
-            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
-            canExecute = CanExecute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
         }
 
-        public override bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+        public override bool CanExecute(object? parameter) => parameter != null && (_canExecute?.Invoke(parameter) ?? true);
 
-        public override void Execute(object? parameter) => execute?.Invoke(parameter);
+        public override void Execute(object? parameter)
+        {
+            if (parameter != null) _execute.Invoke(parameter);
+        }
     }
 }
