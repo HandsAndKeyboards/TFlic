0e91506e0e8a0bd333c2befdd2328f0faaddffa7
vany <ivev152@gmail.com>
2022-12-18 15:40:34 +0400

    реализовал поиск аккаунта по его логину

--- src/server/Organization/Controllers/AccountController.cs
+++ src/server/Organization/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Organization.Controllers.DTO;
+using Organization.Models.Authentication;
 using Organization.Models.Contexts;
 using Organization.Models.Organization.Accounts;
 
@@ -14,19 +15,47 @@ using Microsoft.AspNetCore.Authorization;
 #endif
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 [ApiController]
-[Route("Accounts/{AccountId}")]
+[Route("Accounts/")]
 public class AccountController : ControllerBase
 {
     /// <summary>
+    /// Получение аккаунта с указанным Login
+    /// </summary>
+    [HttpGet("/{AccountLogin}")]
+    public ActionResult<AccountDto> GetAccountByLogin(string AccountLogin)
+    {
+        using var authInfoContext = DbContexts.Get<AuthInfoContext>();
+        var account = authInfoContext.Info
+            .Where(info => info.Login == AccountLogin)
+            .Include(info => info.Account)
+            .Select(info => info.Account)
+            .SingleOrDefault();
+        if (account is null) { return NotFound(); }
+
+        var foundAccount = new Account
+        {
+            Id = account.Id,
+            Name = account.Name,
+            UserGroups = account.GetUserGroups(),
+            AuthInfo = new AuthInfo
+            {
+                Login = AccountLogin,
+                PasswordHash = string.Empty
+            }
+        };
+        return Ok(new AccountDto(foundAccount));
+    }
+    
+    /// <summary>
     /// Получение аккаунта с указанным Id
     /// </summary>
-    [HttpGet]
-    public ActionResult<AccountDto> GetAccount(ulong AccountId)
+    [HttpGet("/{AccountId:long}")]
+    public ActionResult<AccountDto> GetAccountById(long AccountId)
     {
         using var accountContext = DbContexts.Get<AccountContext>();
         
         var account = accountContext.Accounts
-            .Where(acc => acc.Id == AccountId)
+            .Where(acc => acc.Id == (ulong) AccountId)
             .Include(acc => acc.UserGroups)
             .Include(acc => acc.AuthInfo)
             .SingleOrDefault();
@@ -39,7 +68,7 @@ public class AccountController : ControllerBase
     /// <summary>
     /// Изменение данных аккаунта с указанным Id
     /// </summary>
-    [HttpPatch]
+    [HttpPatch("/{AccountId}")]
     public ActionResult<AccountDto> PatchAccount(ulong AccountId, [FromBody] JsonPatchDocument<Account> patch)
     {
         using var accountContext = DbContexts.Get<AccountContext>();
@@ -60,7 +89,7 @@ public class AccountController : ControllerBase
     /// <summary>
     /// Получение списка организаций, в которых состоит указанный аккаунт
     /// </summary>
-    [HttpGet("Organizations")]
+    [HttpGet("/{AccountId}/Organizations")]
     public ActionResult<IEnumerable<ulong>> GetAccountsOrganizations(ulong AccountId)
     {
         using var accountContext = DbContexts.Get<AccountContext>();
