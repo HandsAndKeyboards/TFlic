0732619930363faea7b52d97670011c22a71de07
vany <ivev152@gmail.com>
2022-12-24 06:57:35 +0400

    настроил пути к файлам приложения

--- src/client/Model/Constants/FilesystemPaths.cs
+++ src/client/Model/Constants/FilesystemPaths.cs
@@ -1,17 +1,34 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using TFlic.Model.Configuration;
 
 namespace TFlic.Model.Constants;
 
 public static class FilesystemPaths
 {
-#if !DEBUG
+    static FilesystemPaths()
+    {
+        ProgramRoot = Path.Join(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            ConfigurationUtils.FromConfiguration["program_root_relative_path"]
+        );
+        if (!Directory.Exists(ProgramRoot)) { Directory.CreateDirectory(ProgramRoot!); }
+        
+        AccountFullClass = Path.Join(ProgramRoot, AccountRelativePath);
+    }
+    
     /// <summary> Путь к конфигурационному файлу </summary>
     public static readonly string ConfigDir = Directory.GetCurrentDirectory();
-#else
-    /// <summary> Путь к конфигурационному файлу </summary>
-    public static readonly string ConfigDir = $@"{new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent}\Model\Configuration\";
-#endif
     
     /// <summary> Название конфигурационного файла </summary>
     public const string ConfigName = "configuration.json";
+
+    /// <summary> Путь к директории с файлами программы </summary>
+    public static readonly string ProgramRoot;
+
+    /// <summary> Путь к ключам шифрования </summary>
+    /// <remarks> Путь относителен и начинается с корневой директории программы </remarks>
+    public static readonly string? AccountRelativePath = ConfigurationUtils.FromConfiguration["account_relative_path"];
+
+    public static readonly string? AccountFullClass;
 }
