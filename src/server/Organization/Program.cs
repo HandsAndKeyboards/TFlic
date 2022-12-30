6ef08accfc4b00bc127fbec556ee6482a6d84ff1
vany <ivev152@gmail.com>
2022-12-30 14:48:14 +0400

    теперь swagger доступен и в режиме сборки Production

--- src/server/Organization/Program.cs
+++ src/server/Organization/Program.cs
@@ -81,12 +81,8 @@ public static class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
